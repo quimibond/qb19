5b9f24a1c08318052274242fd705e03eea53ed1f
jsra2025 <joses.ruizarizmendi@gmail.com>
2026-02-16 10:18:36 -0600

    actualizacion de reportes

--- addons/stock_label_custom/report/report_stock_label_custom_zpl.py
+++ addons/stock_label_custom/report/report_stock_label_custom_zpl.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+import socket
+from odoo import models, _
+from odoo.exceptions import UserError
 
 class ReportStockLabelCustomZPL(models.AbstractModel):
     _name = 'report.stock_label_custom.report_stock_label_custom_zpl'
     _description = 'Custom ZPL Label Report'
 
     def _get_report_values(self, docids, data=None):
-        print("DEBUG ZPL REPORT CALLED", docids)
         docs = self.env['stock.move.line'].browse(docids)
         res = []
         for line in docs:
@@ -18,9 +19,27 @@ class ReportStockLabelCustomZPL(models.AbstractModel):
                 'product_qty': line.product_qty or 0,
                 'caja': (lot.name or '')[-4:],
             })
-        print("DEBUG DOCS:", res)    
+
+        # Renderizamos el template QWeb para obtener el ZPL completo
+        zpl_string = self.env['ir.qweb']._render(
+            'stock_label_custom.report_stock_label_custom_zpl',
+            {'docs': res}
+        )
+
+        # Enviamos el ZPL directamente a la impresora Zebra
+        self._send_to_printer('192.168.1.50', 9100, zpl_string)
+
         return {
             'doc_ids': docids,
             'doc_model': 'stock.move.line',
             'docs': res,
         }
+
+    def _send_to_printer(self, ip, port, zpl_data):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, port))
+            s.sendall(zpl_data.encode('utf-8'))
+            s.close()
+        except Exception as e:
+            raise UserError(_("Error enviando a impresora: %s") % e)
