0d337ae7dd28f4aefc13604446c73b6c526a4643
Jose Sacramento Ruiz Arizmendi <joses.ruizarizmendi@gmail.com>
2026-06-11 10:49:29 -0600

    actualizacion v3 js handler pesajes

--- mrp_revisado_telas/static/src/js/scale_revisado_handler.js
+++ mrp_revisado_telas/static/src/js/scale_revisado_handler.js
@@ -8,7 +8,7 @@ patch(FormController.prototype, {
     setup() {
         super.setup(...arguments);
         this.iotLongpollingService = useService("iot_longpolling");
-        this.ormService = useService("orm"); // 🔒 CORREGIDO: Importamos el servicio ORM de Odoo 19
+        this.ormService = useService("orm");
 
         onMounted(() => {
             if (this.model.root.resModel === 'mrp.revisado.wizard') {
@@ -23,11 +23,10 @@ patch(FormController.prototype, {
         });
     },
 
-   _connectToRevisadoScale() {
+    _connectToRevisadoScale() {
         const root = this.model.root;
         if (root.data.weighing_mode === 'iot' && root.data.iot_device_id) {
             
-            // 🔒 SOLUCIÓN MAESTRA: Extraemos el ID numérico real sin importar el formato de Odoo 19
             let iotDeviceId = null;
             const rawData = root.data.iot_device_id;
 
@@ -39,7 +38,6 @@ patch(FormController.prototype, {
                 iotDeviceId = rawData;
             }
 
-            // Forzamos a que sea un entero plano válido antes de enviarlo al ORM
             const finalId = parseInt(iotDeviceId, 10);
 
             if (finalId && !isNaN(finalId)) {
@@ -47,6 +45,7 @@ patch(FormController.prototype, {
                 .then((result) => {
                     if (result && result.length > 0) {
                         const dev = result[0];
+                        
                         this.revisadoScaleListener = (data) => {
                             if (data.status === 'success' && data.value !== undefined) {
                                 if (root.data.peso_actual !== data.value) {
@@ -54,7 +53,13 @@ patch(FormController.prototype, {
                                 }
                             }
                         };
-                        this.iotLongpollingService.addListener(dev.iot_id[1], dev.identifier, this.revisadoScaleListener);
+
+                        // 🔒 CORRECCIÓN ODOO 19: Pasamos un objeto con las propiedades correctas
+                        this.iotLongpollingService.addListener({
+                            iot_ip: dev.iot_id[1],
+                            identifier: dev.identifier,
+                            callback: this.revisadoScaleListener
+                        });
                     }
                 });
             }
